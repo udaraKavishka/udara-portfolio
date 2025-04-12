cf182d9dc336f1f2b129a41a9598168ecb4275e8
Udara Nalawansa <udarakavishka13@gmail.com>
2025-04-13 01:07:09 +0530

    feat: add About component to RootLayout for improved content structure

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Navbar />
         <Hero />
+        <About/>
 
         {children}
       </body>
